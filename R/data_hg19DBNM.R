0f095c01242191dda3212d59ed9ba539f264a7c3
Liang Bo Wang <ccwang002@gmail.com>
2015-05-23 20:58:21 +0800

    add data description

---
+++ R/data_hg19DBNM.R
@@ -0,0 +1,17 @@
+#' Prices of 50,000 round cut diamonds
+#'
+#' A dataset containing the prices and other
+#' attributes of almost 54,000 diamonds
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{marker_id}{price, in US dollars}
+#'   \item{chromosome}{price, in US dollars}
+#'   \item{start}{price, in US dollars}
+#'   \item{stop}{price, in US dollars}
+#'   \item{strand}{price, in US dollars}
+#'   \item{Gene.Symbol}{price, in US dollars}
+#'   \item{Transcript}{test}
+#' }
+#' @source \url{UCSC Genome Browser}
+"hg19DBNM"
