675f76061deaeb4fde032709e4b80cf362c8d997
yplai <yipinglai1988@gmail.com>
2015-05-26 22:00:44 +0800

    Documenting datasets of hg19-RefSeq

--- R/data_hg19DBNM.R
+++ R/data_hg19DBNM.R
@@ -1,17 +1,21 @@
-#' Prices of 50,000 round cut diamonds
+#' The human genome reference used here is RefSeq transcripts in version hg19, hg19-RefSeq,
+#' from UCSC Genome Browser.
 #'
-#' A dataset containing the prices and other
-#' attributes of almost 54,000 diamonds
+#' This reference provides region information, including chromosome number, starting position,
+#' ending position, strand and gene symbols, for converting copy number alteration data into human genes.
 #'
-#' @format A data frame with 53940 rows and 10 variables:
+#' The transcripts with NM marker ID, which are protein-codeing, were selected to be our reference database
+#' and provided as hg19DBNM.rda.
+#'
+#' @format A data frame with 39997 rows and 7 variables:
 #' \describe{
-#'   \item{marker_id}{price, in US dollars}
-#'   \item{chromosome}{price, in US dollars}
-#'   \item{start}{price, in US dollars}
-#'   \item{stop}{price, in US dollars}
-#'   \item{strand}{price, in US dollars}
-#'   \item{Gene.Symbol}{price, in US dollars}
-#'   \item{Transcript}{test}
+#'   \item{marker_id}{RefSeq name with its corrsponding gene symbol}
+#'   \item{chromosome}{1-21, X and Y}
+#'   \item{start}{starting position, in basepair number}
+#'   \item{stop}{ending position, in basepair number}
+#'   \item{strand}{positive or negative strand, in + or - symbols}
+#'   \item{Gene.Symbol}{Gene name}
+#'   \item{Transcript}{RefSeq name}
 #' }
-#' @source \url{UCSC Genome Browser}
+#' @source \url{UCSC Genome Browser, http://hgdownload.cse.ucsc.edu/downloads.html}
 "hg19DBNM"
