f962d25291ab5fa35447934db769d563e0c90717
Liang Bo Wang <ccwang002@gmail.com>
2015-06-26 13:47:21 +0800

    fix iGC.R
    ...

--- R/iGC.R
+++ R/iGC.R
@@ -1,15 +1,10 @@
 #' iGC: an integrated analysis package of gene expression and copy number
 #' alteration
 #'
-<<<<<<< HEAD
-#' The iGC package is used to identify CNA-driven differentially expressed genes.
-#' The iGC package provides four categories of important functions:
-#' create_sample_desc, create_gene_exp, faster_gene_cna and find_cna_drive_gene.
-=======
 #' The iGC package is used to identify CNA-driven differentially expressed
 #' genes. The iGC package provides four categories of important functions:
-#' create_sample_desc, create_gene_exp, creat_gene_cna and find_cna_drive_gene.
->>>>>>> bb0b69bc216f60ef9bb81b67b05100452d087645
+#' `create_sample_desc`, `create_gene_ex`, `create_gene_cna` and
+#' `find_cna_drive_gene`.
 #'
 #' @section create_sample_desc: The create_sample_desc function is provided for
 #'   creating sample description table containing all required inputs.
@@ -19,19 +14,12 @@
 #'   rearrange the input gene expression files to a gene expression list of
 #'   entire samples.
 #'
-<<<<<<< HEAD
-#' @section faster_gene_cna function:
-#' The faster_gene_cna function maps CNA data to human genes and then defines the mapped human genes as CN gain or loss
-#' based on the CN threshold, whose default values are set as 2.5 for gain and 1.5 for loss. These mapped genes will
-#' be assigned values in +1, -1 or 0, where +1 stands for CNA-gain, -1 stands for CNA-loss and 0 stands for neutral.
-=======
 #' @section creat_gene_cna function: The creat_gene_cna function maps CNA data
 #'   to human genes and then defines the mapped human genes as CN gain or loss
 #'   based on the CN threshold, whose default values are set as 2.5 for gain and
 #'   1.5 for loss. These mapped genes will be assigned values in +1, -1 or 0,
 #'   where +1 stands for CNA-gain, -1 stands for CNA-loss and 0 stands for
 #'   neutral.
->>>>>>> bb0b69bc216f60ef9bb81b67b05100452d087645
 #'
 #' @section find_cna_drive_gene function: The find_cna_drive_gene function
 #'   identifies CNA-driven differentially expressed genes. The input mapped
